732907ea3ff70eb7beefd98519973b7223b6d151
pichu07 <pranjal.yadav@cawstudios.com>
2022-07-07 13:00:48 +0530

    reverting back to original main

--- components/heartbeat/Repositories/heartbeat.Repository.ts
+++
@@ -1,8 +0,0 @@
-import { HeartbeatModel } from "../../../common/models/heartbeat.Model";
-
-export class HeartbeatRepository {
-  public getBeat(): HeartbeatModel {
-    const date = new Date();
-    return new HeartbeatModel(date);
-  }
-}
--- components/heartbeat/Services/heartbeat.Service.ts
+++
@@ -1,8 +0,0 @@
-import { HeartbeatRepository } from "../Repositories/heartbeat.Repository";
-
-export class HeartbeatServices {
-  public getBeat(): any {
-    const heartbeatRepositoryInstance = new HeartbeatRepository();
-    return heartbeatRepositoryInstance.getBeat();
-  }
-}
