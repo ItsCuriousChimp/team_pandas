39210e6a42bc5ad7f2332bb189ff0ed3947a54d4
Shouryamup <73136513+Shouryamup@users.noreply.github.com>
2022-07-18 13:24:15 +0530

    changed router syntax

--- src/routes/theatre.route.ts
+++ src/routes/theatre.route.ts
@@ -4,8 +4,8 @@ import { Router } from "express";
 const router = Router();
 // view availability status of each show for a particular theatre and movie
 
-// router.route('/:theatreId/show').get([TheatreControllerInstance.getMovieShows]);
-router.get("/:theatreId/show", TheatreControllerInstance.getMovieShows);
+router.route("/:theatreId/show").get([TheatreControllerInstance.getMovieShows]);
+// router.get("/:theatreId/show", TheatreControllerInstance.getMovieShows);
 
 // view availabile seats of a particular showTime
 router
