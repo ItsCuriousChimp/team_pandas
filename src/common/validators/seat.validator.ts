bbb1dea145ad97460f6e1cc357cbc6769ea26751
Shouryamup <73136513+Shouryamup@users.noreply.github.com>
2022-07-29 01:04:57 +0530

    addition of validations

--- src/common/validators/seat.validator.ts
+++ src/common/validators/seat.validator.ts
@@ -1,5 +1,11 @@
 import Joi from "joi";
 
 export const seatValidation = {
-  body: Joi.object({}),
+  query: Joi.object({
+    movieId: Joi.string().required().guid(),
+    showId: Joi.string().required().guid(),
+  }),
+  params: Joi.object({
+    theatreId: Joi.string().required().guid(),
+  }),
 };
--- src/common/validators/show.validator.ts
+++ src/common/validators/show.validator.ts
@@ -1,5 +1,10 @@
 import Joi from "joi";
 
 export const showValidation = {
-  body: Joi.object({}),
+  query: Joi.object({
+    movieId: Joi.string().required().guid(),
+  }),
+  params: Joi.object({
+    theatreId: Joi.string().required().guid(),
+  }),
 };
