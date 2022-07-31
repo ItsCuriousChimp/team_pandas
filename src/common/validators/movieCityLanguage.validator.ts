24c121e05d482755b1fef2e9053b6e8511ada405
pichu07 <pranjal.yadav@cawstudios.com>
2022-08-01 03:43:51 +0530

    Pull code from branch and resolve merge conflict

--- src/common/validators/movieCityLanguage.validator.ts
+++
@@ -1,8 +0,0 @@
-import Joi from "joi";
-
-export const movieCityLanguageValidation = {
-  query: Joi.object({
-    language: Joi.string().required(),
-    cityId: Joi.string().required(),
-  }),
-};
