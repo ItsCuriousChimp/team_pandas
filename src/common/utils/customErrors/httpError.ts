4e1cd73e0489e7112f161661ad130a1557ecaf33
Pranjal Yadav <108271173+pichu07@users.noreply.github.com>
2022-08-02 18:00:57 +0530

    [ TP- 44 ] Login API (#22)
    * Login Api created
    
    * Login Api created
    
    * Typo fix in index file
    
    * Exporting instance instead of class in repository and service files
    
    * Adding validation
    
    * Changing logger messages and adding loggers wherever necessary
    
    * Using redis to store jwt token
    
    * Handling validation error in middleware
    
    * Removing unnecessary logs
    
    * Changing middleware file name
    
    * Changing logger name
    
    * Shifting logic from repository to service
    
    * Sending filename in loggers
    
    * Changing repository and service filename and creating user repository
    
    * Adding loggers for successful response
    
    * Adding custom error handlers and global redis client
    
    * Renaming apiRequestError to apiError
    
    * Handle custom errors and change file names
    
    * Rename filenames and solve redis client issues
    
    * Add redis client to storage and optimize auth service
    
    * Handle database error in dbClient
    
    * Typo fix in custom authentication error
    
    * Remove port from constants
    
    * Move dotenv to index
    
    * Parameters shift to super and authentication file spelling change
    
    * Set errorType on top

---
+++ src/common/utils/customErrors/httpError.ts
@@ -0,0 +1,14 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import CustomError from "./customError";
+
+class HTTPError extends CustomError {
+  errorType = this.constructor.name;
+  constructor(message: string, err: any) {
+    super({
+      message: err.message,
+      stack: err.stack,
+      statusCode: err.status,
+    });
+  }
+}
+export default HTTPError;
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,19 @@
-import express, { Express, Response, Request } from "express";
+import express, { Express, Response, Request, NextFunction } from "express";
+import bodyParser from "body-parser";
+import * as dotenv from "dotenv";
+import * as error from "./middleware/error.middleware";
+import { loginRouter } from "./routes/login.route";
 import { heartbeatController } from "./controllers/heartbeat.controller";
-const PORT = 3000;
+
 const app: Express = express();
+
+dotenv.config();
+
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.json());
 
 app.get("/", (req: Request, res: Response) => {
   res.send("HELLO WORLD! get your heartbeat from /heartbeat");
@@ -9,7 +21,19 @@ app.get("/", (req: Request, res: Response) => {
 
 app.get("/heartbeat", heartbeatController.getTimeStamp);
 
-app.listen(process.env.NODE_ENV || PORT, () => {
+app.use("/login", loginRouter);
+
+// Error handler middleware
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  next(err);
+});
+
+app.use(error.handler);
+
+// Handle 404 error
+app.use(error.resourceNotFoundHandler);
+
+app.listen(process.env.PORT, () => {
   // eslint-disable-next-line no-console
-  console.log(`Server running at PORT ${PORT}`);
+  console.log(`Server running at PORT ${process.env.PORT}`);
 });
