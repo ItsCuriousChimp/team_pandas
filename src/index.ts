2004f4027c74f15b3abc9517ba833469ded21a29
pichu07 <pranjal.yadav@cawstudios.com>
2022-08-11 14:55:52 +0530

    Add whitelist urls to cors

--- src/index.ts
+++ src/index.ts
@@ -16,7 +16,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.json());
 
-app.use(cors());
+app.use(cors({ origin: process.env.WHITELIST_URLS }));
 
 app.get("/", (req: Request, res: Response) => {
   res.send("HELLO WORLD! get your heartbeat from /heartbeat");
