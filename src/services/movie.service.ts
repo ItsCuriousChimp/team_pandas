00b9821f61e4762542086fbae2eec0ee7cc88ce2
pichu07 <pranjal.yadav@cawstudios.com>
2022-08-11 14:35:50 +0530

    Remove unnecessary try and catch block

--- src/services/movie.service.ts
+++ src/services/movie.service.ts
@@ -1,6 +1,5 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable no-useless-catch */
-import logger from "../common/logger/logger";
 import CustomError from "../common/utils/customErrors/customError";
 import { Movie } from "../models/movie.model";
 import { movieRepository } from "../repositories/movie.repository";
@@ -14,30 +13,16 @@ class MovieService {
     language: string,
     cityId: string
   ): Promise<Movie[]> => {
-    try {
-      if (!(await this.isCityValid(cityId)))
-        throw new CustomError({
-          message: "Unable to find city Id",
-          statusCode: 400,
-          data: cityId,
-        });
-
-      const movies = await movieRepository.getMoviesByLanguage(
-        language,
-        cityId
-      );
-
-      return movies;
-    } catch (err: any) {
-      logger.error({
+    if (!(await this.isCityValid(cityId)))
+      throw new CustomError({
+        message: "Unable to find city Id",
+        statusCode: 400,
         data: cityId,
-        message: "Unable to filter movies",
-        error: err,
-        __filename,
       });
 
-      throw err;
-    }
+    const movies = await movieRepository.getMoviesByLanguage(language, cityId);
+
+    return movies;
   };
 }
 export const movieService = new MovieService();
